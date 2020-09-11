0568768d6b42bf7e53410bdd8a5d6f8b34a62f3d
caetano <joaoluisfcaetano@gmail.com>
2020-09-11 09:50:20 -0300

    Creating the function that generates the nav bar

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.getElementsByTagName("section");
+const navMenu = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
@@ -34,7 +35,27 @@
 */
 
 // build the nav
+function buildNavMenu() {
+    const fragment = document.createDocumentFragment();
+    
+    for (let section of sections) {
+        console.log(section);
+        const navItem = document.createElement("li");
+        navItem.className = "menu__link";
+        navItem.innerText = section.getAttribute("data-nav");
+        fragment.appendChild(navItem);
+    }
 
+    /* for (let i = 1; i <= sections.length; i++) {
+        console.log(sections[i-1]);
+        const navItem = document.createElement("li");
+        navItem.className = "menu__link";
+        navItem.innerText = `Section ${i}`;
+        fragment.appendChild(navItem);
+    } */
+
+    navMenu.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
 
