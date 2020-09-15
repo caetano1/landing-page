f5dd1732c7bea3cdc58dbda4d3007f1103ec4d89
caetano <joaoluisfcaetano@gmail.com>
2020-09-14 21:14:52 -0300

    Adding smooth behavior // Adding active status

--- js/app.js
+++ js/app.js
@@ -26,6 +26,33 @@ const navMenu = document.getElementById("navbar__list");
  * 
 */
 
+// Identify if the element is in the viewport or not
+function isInViewport (element) {
+    /* // defines the top and bottom borders
+    const topBorder = element.offsetTop;
+    const bottomBorder = topBorder + element.offsetHeight;
+
+    // defines the Y coordinate of the viewport and its height
+    const topViewport = window.pageYOffset
+    const bottomViewport = topViewport + window.innerHeight */
+
+    const viewportHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const percLimit = 0.5;
+
+    let boundaries = element.getBoundingClientRect();
+
+    /* 
+    To do: Check later how to solve for elements that
+    occupy less than 50% of the viewport
+    */
+
+    // If the element starting point is above the viewport
+    if (boundaries.top < 0) {
+        return (boundaries.bottom / viewportHeight > percLimit);
+    } else {
+        return ((viewportHeight - boundaries.top)/viewportHeight > percLimit);
+    }
+}
 
 
 /**
@@ -35,33 +62,75 @@ const navMenu = document.getElementById("navbar__list");
 */
 
 // build the nav
-function buildNavMenu() {
+/* function buildNavMenu() { */
+const buildNavMenu = function () {
     const fragment = document.createDocumentFragment();
     
-    for (let section of sections) {
+    /* for (let section of sections) {
         console.log(section);
         const navItem = document.createElement("li");
         navItem.className = "menu__link";
         navItem.innerText = section.getAttribute("data-nav");
-        fragment.appendChild(navItem);
-    }
+        navItem.addEventListener("click", function(e) {
+            console.log(e);
+            section.scrollIntoView({behavior: "smooth"});
+        })
 
-    /* for (let i = 1; i <= sections.length; i++) {
-        console.log(sections[i-1]);
-        const navItem = document.createElement("li");
-        navItem.className = "menu__link";
-        navItem.innerText = `Section ${i}`;
         fragment.appendChild(navItem);
     } */
+
+    for (let i = 1; i <= sections.length; i++) {
+        console.log(sections[i-1]);
+        const navItem = document.createElement("li");
+        const navLink = document.createElement("a");
+
+        navItem.className = "menu__item";
+        navLink.className = "menu__link";
+        navLink.setAttribute("href", `#section${i}`)
+        navLink.innerText = `Section ${i}`;
+        navItem.appendChild(navLink);
+
+        fragment.appendChild(navItem);
+    }
 
     navMenu.appendChild(fragment);
 }
 
 // Add class 'active' to section when near top of viewport
-
+window.addEventListener ("scroll", function () {
+    // Loops through each section and checks if it is in the viewport
+    for (const section of sections) {
+        if (isInViewport(section)) {
+            /* console.log(section);
+            console.log("active"); */
+            section.classList.add("active");
+        } else {
+            /* console.log(section);
+            console.log("inactive") */
+            section.classList.remove("active");
+        }
+    }
+})
 
 // Scroll to anchor ID using scrollTO event
-
+    
+// Fetches each menu item and apply an eventlistener to it
+const navItems = document.getElementsByClassName("menu__link");
+/* console.log(navItems, 1); */
+function smoothBehavior (items, anchor) {
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener ("click", function(e) {
+            /* console.log(items[i]);
+            console.log(e);
+            console.log(anchor[i]); */
+            e.preventDefault();
+            window.scrollTo ({
+                top: anchor[i].offsetTop,
+                behavior: "smooth"
+            });
+        });
+    }
+};
 
 /**
  * End Main Functions
@@ -70,8 +139,11 @@ function buildNavMenu() {
 */
 
 // Build menu 
+/* window.addEventListener("DOMContentLoaded", buildNavMenu); */
+buildNavMenu();
 
 // Scroll to section on link click
+smoothBehavior(navItems, sections);
 
 // Set sections as active
 
