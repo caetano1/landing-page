cf24ac6646751c4b5aa723fa5f424466dd9078a6
caetano <joaoluisfcaetano@gmail.com>
2020-09-20 20:31:11 -0300

    Adjusting styles and active sections

--- js/app.js
+++ js/app.js
@@ -28,25 +28,13 @@ const navMenu = document.getElementById("navbar__list");
 
 // Identify if the element is in the viewport or not
 function isInViewport (element) {
-    /* // defines the top and bottom borders
-    const topBorder = element.offsetTop;
-    const bottomBorder = topBorder + element.offsetHeight;
-
-    // defines the Y coordinate of the viewport and its height
-    const topViewport = window.pageYOffset
-    const bottomViewport = topViewport + window.innerHeight */
 
     const viewportHeight = (window.innerHeight || document.documentElement.clientHeight);
     const percLimit = 0.5;
 
     let boundaries = element.getBoundingClientRect();
 
-    /* 
-    To do: Check later how to solve for elements that
-    occupy less than 50% of the viewport
-    */
-
-    // If the element starting point is above the viewport
+    // If the element starting point is above the viewport, or else
     if (boundaries.top < 0) {
         return (boundaries.bottom / viewportHeight > percLimit);
     } else {
@@ -62,23 +50,10 @@ function isInViewport (element) {
 */
 
 // build the nav
-/* function buildNavMenu() { */
 const buildNavMenu = function () {
     const fragment = document.createDocumentFragment();
-    
-    /* for (let section of sections) {
-        console.log(section);
-        const navItem = document.createElement("li");
-        navItem.className = "menu__link";
-        navItem.innerText = section.getAttribute("data-nav");
-        navItem.addEventListener("click", function(e) {
-            console.log(e);
-            section.scrollIntoView({behavior: "smooth"});
-        })
 
-        fragment.appendChild(navItem);
-    } */
-
+    // runs a loop through each section and add the corresponding link to the header
     for (let i = 1; i <= sections.length; i++) {
         console.log(sections[i-1]);
         const navItem = document.createElement("li");
@@ -101,12 +76,8 @@ window.addEventListener ("scroll", function () {
     // Loops through each section and checks if it is in the viewport
     for (const section of sections) {
         if (isInViewport(section)) {
-            /* console.log(section);
-            console.log("active"); */
             section.classList.add("active");
         } else {
-            /* console.log(section);
-            console.log("inactive") */
             section.classList.remove("active");
         }
     }
@@ -116,13 +87,10 @@ window.addEventListener ("scroll", function () {
     
 // Fetches each menu item and apply an eventlistener to it
 const navItems = document.getElementsByClassName("menu__link");
-/* console.log(navItems, 1); */
+
 function smoothBehavior (items, anchor) {
     for (let i = 0; i < items.length; i++) {
         items[i].addEventListener ("click", function(e) {
-            /* console.log(items[i]);
-            console.log(e);
-            console.log(anchor[i]); */
             e.preventDefault();
             window.scrollTo ({
                 top: anchor[i].offsetTop,
@@ -139,12 +107,9 @@ function smoothBehavior (items, anchor) {
 */
 
 // Build menu 
-/* window.addEventListener("DOMContentLoaded", buildNavMenu); */
 buildNavMenu();
 
 // Scroll to section on link click
 smoothBehavior(navItems, sections);
-
-// Set sections as active
 
 
